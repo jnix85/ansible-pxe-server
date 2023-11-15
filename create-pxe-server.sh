c443c8b5fdd4a10963f8d16621fe195ec20f8b0f
Pup Gizmo <149908413+gizmohundur@users.noreply.github.com>
2023-11-15 11:26:36 -0600

    .

--- create-pxe-server.sh
+++ create-pxe-server.sh
@@ -17,7 +17,7 @@ virsh undefine --nvram --remove-all-storage pxe
 ## Create VM, 2 vCPUs, 4GB RAM, bridge networking, with a Kickstart file to automate install
 virt-install --name pxe \
     --memory 4096 \
-    --vcpus "sockets=1,cores=2,threads=1" \
+    --vcpus 2 \
     --location ${INSTALL_URL} \
     --extra-args 'console=tty0 console=ttyS0 nameserver=10.1.0.2 systemd.zram=0 ip=10.1.0.8::10.1.0.1:255.255.255.0:boot.home.jpconsulted.com:enp1s0:none inst.ks=http://10.1.0.2/pxe.ks' \
     --os-variant almalinux9 \
